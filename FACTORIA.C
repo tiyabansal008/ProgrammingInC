f84d7dc7f0fabc626d4e81a4cbe62d7eefcb49b2
Tiya Bansal <tiyabansal008@gmail.com>
2026-08-29 12:25:39 +0530

    my factorial program

--- FACTORIA.C
+++ FACTORIA.C
@@ -1,3 +1,9 @@
+/*******************************************
+Statement - Print Hello World
+Programmer - Tiya Bansal
+Written For - Programming lab (1st Sem)
+********************************************/
+
 #include <stdio.h>
 int main()
 {
